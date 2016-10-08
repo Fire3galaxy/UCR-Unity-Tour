715f42686b1fd6db83ce098a2f64ab671cbc9087
Fire3galaxy <daniel_handojo@hotmail.com>
2016-10-08 01:22:44 -0700

    master branch is safe currently

---
+++ Another Stream/Assets/_Scripts/AttachToCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class AttachToCamera : NetworkBehaviour {
+    
+	void Start () {
+        if (isLocalPlayer)
+            Camera.main.GetComponent<PlayerHolder>().playerObject = gameObject;
+	}
+}
