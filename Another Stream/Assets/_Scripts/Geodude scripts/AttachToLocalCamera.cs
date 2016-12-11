017f7ad04480c4700c2798c39723f46436fa1966
Fire3galaxy <daniel_handojo@hotmail.com>
2016-12-10 23:03:15 -0800

    Added "Sync Scene" scene and button

---
+++ Another Stream/Assets/_Scripts/Geodude scripts/AttachToLocalCamera.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttachToLocalCamera : Photon.MonoBehaviour
+{
+    void Update()
+    {
+        if (PhotonNetwork.masterClient.isLocal)
+            transform.rotation = Camera.main.GetComponent<Transform>().rotation;
+    }
+}
---
+++ Another Stream/Assets/_Scripts/PUN scripts/InstantiateAvatar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateAvatar : MonoBehaviour {
+    public GameObject avatarPrefab;
+    private GameObject avatarGameObject;
+
+    public void OnJoinedRoom()
+    {
+        avatarGameObject = PhotonNetwork.Instantiate(avatarPrefab.name, new Vector3(0, 0, 0), Quaternion.identity, 0);
+        if (avatarGameObject != null)
+            avatarGameObject.GetComponent<Renderer>().material.color = Color.cyan;
+    }
+}
