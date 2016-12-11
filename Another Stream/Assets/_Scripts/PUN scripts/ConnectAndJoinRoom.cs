e394390437ace8aad4b48d70b87dd056f3ddc942
Fire3galaxy <daniel_handojo@hotmail.com>
2016-12-11 00:16:31 -0800

    Made connect to photon script
    connects and checks room list

---
+++ Another Stream/Assets/_Scripts/PUN scripts/ConnectAndJoinRoom.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConnectAndJoinRoom : Photon.MonoBehaviour {
+    public byte Version = 1;
+    private bool ConnectedInUpdate = false;
+
+	// Use this for initialization
+	public virtual void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	public virtual void Update ()
+    {
+        if (!ConnectedInUpdate)
+        {
+            Debug.Log("In update");
+            PhotonNetwork.ConnectUsingSettings(Version + ".0");
+            ConnectedInUpdate = true;
+        }
+    }
+
+    public virtual void OnJoinedLobby()
+    {
+        Debug.Log("Joined Lobby");
+        RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+
+        if (rooms.Length == 0)
+            Debug.Log("No rooms available");
+        else
+        {
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                Debug.Log(rooms[i].name);
+            }
+        }
+    }
+}
--- Another Stream/Assets/_Scripts/Scene scripts/ChangeScene.cs
+++ Another Stream/Assets/_Scripts/Scene scripts/ChangeScene.cs
@@ -3,19 +3,10 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class ChangeScene : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public string sceneName;
 
     public void Move()
     {
-        
+        SceneManager.LoadSceneAsync(sceneName);
     }
 }
